d674da09049c807a266f7ebee0cca600afddaa2b
marentes <marentes_199@hotmail.com>
2019-11-12 20:47:15 -0600

    Login AUTH::0

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.next-steps {
+  .fa-link {
+    margin-right: 5px;
+  }
+}
+</style>
